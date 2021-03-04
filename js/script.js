dbe822da01a4fed3e4b7a2118a77f284823e9115
Katherine Gomez <kathygomez26@hotmail.com>
2021-03-04 10:09:00 -0600

    Add files via upload
    displays all pokemon stats on webpage

--- js/script.js
+++ js/script.js
@@ -1,34 +1,55 @@
-// DOM Objects
+//A variable is created with the 'poke_container' id
+const poke_container = document.getElementById('poke_container');
+const pokemons_number = 151;
 
-const pokemonName = document.querySelector('.poke-name');
-const pokemonId = document.querySelector('.poke-id');
-const pokemonWeight = document.querySelector('.poke-weight');
-const pokemonHeight = document.querySelector('.poke-height')
-const pokemonTypeOne = document.querySelector('.poke-type-one');
-const pokemonSprite = document.querySelector('.poke-front-image');
+//With the 'fetchPokemons' function you use a for loop to cycle 
+//through the pokemons until we reach the number declared in 
+//the previous 'pokemons_number' variable
 
-//fetching the Poke API
-fetch('https://pokeapi.co/api/v2/pokemon/7')
-.then(res => res.json())
-.then(data =>{
-   console.log(data);
+const fetchPokemons = async () => {
+   for (let i = 1; i <= pokemons_number; i++) {
+      await getPokemon(i);
+   }
+};
 
+//The Async function allows for asycnchronous, 
+//promise-based behaviors instead of having to create 
+//synchronous (.then) promise chains to call the API
 
-//calling the data stats parameters
-   pokemonName.textContent = data['name'];
-   pokemonId.textContent = data['id'];
-   pokemonWeight.textContent = data['weight'];
-   pokemonHeight.textContent = data['height']
-   
-   const dataTypes = data['types'];
-   const dataFirstType = dataTypes[0];
-   pokemonTypeOne.textContent = dataFirstType['type']['name'];
-  
+const getPokemon = async id => {
+   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+   const res = await fetch(url);
+   const pokemon = await res.json();
+   createPokemonCard(pokemon);
+}
 
-   pokemonSprite.src = data['sprites']['front_default'] || '';
+//With the createPokemonCard function, a new div is created with 
+//the pokemon class, below indicates what's contained within the 
+//pokemon class
 
-});
-   
+const createPokemonCard = (pokemon) => {
+   const pokemonEl = document.createElement('div');
+   pokemonEl.classList.add('pokemon');
+   const {id, name, height, weight, sprites, types} = pokemon;
+   const type = types[0].type.name;
 
+   // The pokeInnerHTML is made to call the pokemon stats we 
+   //just set to the pokemon variable and tell it how to display 
+   //on the website 
+   const pokeInnerHTML = `<div class = "img-container">
+   <img src="${sprites.front_default}" alt="${name}"/></div>
+   <div class = "stats">
+      <span class = "number">${id}</span>
+      <h3 class = "name">${name}</h3>
+      <small class = "height">${height}</small>
+      <small class = "weight">${weight}</small>
+      <small class = "type">Type:
+      <span>${type}</span></small>
+      </div>
+      `;
+   pokemonEl.innerHTML = pokeInnerHTML;
+   poke_container.appendChild(pokemonEl);
+}
 
+fetchPokemons();
 
